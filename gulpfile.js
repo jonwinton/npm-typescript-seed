8e1c488716baf8e099b0cf2d399ff3f2d301d856
Jon Winton <winton.jon@gmail.com>
2016-05-18 11:57:39 -0400

    Adding a lint task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,23 @@
 
 var gulp = require('gulp');
 var typescript = require('gulp-tsc');
+var tslint = require('gulp-tslint');
 
-gulp.task('compile', function(){
+gulp.task('compile', function() {
   gulp.src(['src/**/*.ts'])
     .pipe(typescript())
     .pipe(gulp.dest('dist/'))
+});
+
+/**
+ * Lint
+ *
+ * `gulp-tslint` automatically searches for the
+ * `tslint.json` file in the same directory, so there's
+ * no need to provide an explicit path.
+ */
+gulp.task('lint', function() {
+  gulp.src(['src/**/*.ts'])
+    .pipe(tslint())
+    .pipe(tslint.report('verbose'))
 });
